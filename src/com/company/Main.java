4f9ba5c43f0d7b761de06fb95a75a8c3b61ca990
as-aijamal <a.asangazieva@gmail.com>
2022-01-09 03:06:10 +0600

    First Stage Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        Car.makeCar();
+
     }
 }
